a6ccdb8bb85798573a8066b782b2e6c483ce129e
lee <litao1009@gmail.com>
2016-08-16 14:23:12 +0800

    +sdl -use sdl for event loop

--- KapokCPPServer.Main/main.cpp
+++ KapokCPPServer.Main/main.cpp
@@ -1,12 +1,26 @@
+#include "vld.h"
+
 #include "TCPServer.h"
 #include "TcpSession.h"
 #include "AsyncThreadPool.h"
 
+#include "SDL.h"
+
 #include <vector>
 #include <iostream>
 
+#ifdef main
+#undef main
+#endif // main
+
 int main()
 {
+	{
+		auto ret = SDL_Init(SDL_INIT_EVENTS);
+		auto window = SDL_CreateWindow("", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+		auto renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	}
+
 	std::vector<TcpSessionSPtr> sessionList;
 
 	AsyncThreadPool threadPool;
@@ -39,6 +53,28 @@ int main()
 	});
 
 	tcpserver.StartAccept( 13 );
+
+	{
+		SDL_Event evt;
+		auto quit = false;
+		while ( !quit )
+		{
+			while ( SDL_WaitEvent(&evt) )
+			{
+				if ( (evt.type == SDL_KEYDOWN && evt.key.keysym.sym == SDLK_ESCAPE)
+					|| evt.type == SDL_QUIT )
+				{
+					quit = true;
+					break;
+				}
+			}
+		}
+
+		SDL_Quit();
+	}
+
+	tcpserver.StopAccept();
+	threadPool.Stop();
 
 	threadPool.Join();
 }
